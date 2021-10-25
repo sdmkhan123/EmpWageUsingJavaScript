49157164b016b651268c8c717ffba9c3dc00d635
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-26 00:32:22 +0530

    Refactor Function To Get Work Hr

--- empWage.js
+++ empWage.js
@@ -10,7 +10,7 @@ else
 {
     console.log("Employee is absent");
 }
-//Calculate Daily Employee Wage.
+//Refactor the code to Calculate Daily Employee Wage using fun.
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HR = 4;
@@ -18,17 +18,17 @@ const FULL_TIME_HR = 8;
 const WAGE_PER_HR = 20;
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) 
-{
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HR;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HR;
-        break;
-    default:
-        empHrs = 0;
-        break;
+function getWorkingHrs(empCheck) {
+    switch (empCheck) 
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HR;
+        case IS_FULL_TIME:
+            return FULL_TIME_HR;
+        default:
+            return 0;
+    }
 }
+empHrs = getWorkingHrs(empCheck)
 let empWage = empHrs * WAGE_PER_HR;
 console.log("Daily Employee wage: " + empWage);
