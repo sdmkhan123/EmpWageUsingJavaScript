f5ac74aeec2d18917cd5422abaf4c5eeb1aae903
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-25 23:52:57 +0530

    To check Employee is present or absent

--- empWage.js
+++ empWage.js
@@ -1,1 +1,12 @@
 console.log("Welcome to Employee Wage Practice problem");
+//To check Employee is present or absent
+const IS_PRESENT = 1;
+let Check = Math.floor(Math.random() * 10) % 2;
+if (Check == IS_PRESENT) 
+{
+    console.log("Employee is present");
+}
+else 
+{
+    console.log("Employee is absent");
+}
